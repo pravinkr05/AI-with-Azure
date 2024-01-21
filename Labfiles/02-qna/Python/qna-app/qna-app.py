d43f63107e8b2325281bf1a1a59b7a4f7c54e268
Pravin <128983635+pravinkr05@users.noreply.github.com>
2024-01-21 23:28:17 +0530

    First Commit

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-# Import namespaces
-
+ # import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 def main():
     try:
@@ -14,9 +15,23 @@ def main():
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
+         # Create client using endpoint and key
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
 
         # Submit a question and display the answer
+         # Submit a question and display the answer
+        user_question = ''
+        while user_question.lower() != 'quit':
+            user_question = input('\nQuestion:\n')
+            response = ai_client.get_answers(question=user_question,
+                                            project_name=ai_project_name,
+                                            deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
 
 
 
